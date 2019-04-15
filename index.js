79e7ceb9e6b3e009268f86db295cd54bdd44dd90
salapjaka <yulia.murzakhanova@gmail.com>
2019-04-15 18:36:06 -0400

    almost there

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function beginGame(a) {
   }
   console.log(a)
 
+
   for (let i = 0; i < a.length; i++) {
     for (let j = 0; j < a[i].length; j++) {
       if (a[i][j] === 'B') {
@@ -36,10 +37,46 @@ function beginGame(a) {
         !a[i - 1] || isNaN(a[i - 1][j + 1]) ? '' : a[i - 1][j + 1]++ //right & up
 
       }
+    
     }
   }
 }
+window.what = 'hiiii'
 
+function emptySpace (a){ 
+  let allButtons = [
+    document.querySelectorAll('#boardDiv > div:nth-child(1) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(2) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(3) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(4) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(5) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(6) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(7) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(8) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(9) button'),
+    document.querySelectorAll('#boardDiv > div:nth-child(10) button'),
+  ]
+
+  //let allButtons = document.querySelector('button')
+  console.log(allButtons)
+
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a[i].length; j++) {
+      if (a[i][j] === 0) {
+        //console.log(i,j)
+        //console.log(allButtons[i][j - 1], allButtons[0][5])
+        !(allButtons[i][j - 1]) ? '' : allButtons[i][j - 1].click() //right
+        !(allButtons[i][j + 1]) ? '' : allButtons[i][j + 1].click() //left
+        !allButtons[i + 1] || !(allButtons[i + 1][j]) ? '' : allButtons[i + 1][j].click() //down
+        !allButtons[i - 1] || !(allButtons[i - 1][j]) ? '' : allButtons[i - 1][j].click() //up
+        !allButtons[i + 1] || !(allButtons[i + 1][j + 1]) ? '' : allButtons[i + 1][j + 1].click() //right & down
+        !allButtons[i - 1] || !(allButtons[i - 1][j - 1]) ? '' : allButtons[i - 1][j - 1].click() //left & up
+        !allButtons[i + 1] || !(allButtons[i + 1][j - 1]) ? '' : allButtons[i + 1][j - 1].click() //left & down
+        !allButtons[i - 1] || !(allButtons[i - 1][j + 1]) ? '' : allButtons[i - 1][j + 1].click() //right & up
+    } 
+   } 
+  } 
+}
 let board = document.getElementById("boardDiv")
 let rows = 10
 let size = 100 / rows
@@ -50,34 +87,74 @@ function makeField(a) {
     let row = document.createElement("div")
     for (x = 0; x < rows; x++) {
       let button = document.createElement("button");
-      button.addEventListener("click", doSomething);
+      document.addEventListener('contextmenu', event => event.preventDefault());
+      button.addEventListener("click", doSomething)
+      button.oncontextmenu = markBomb;
+      button.setAttribute('toggle',1)//setting the toggle to 1 or odd
       button.value = a[i][x]
       button.setAttribute('value', a[i][x])
-      button.innerHTML = ':)'
       button.style.width = size + "%" //like 20% width
       button.style.height = size + "%"
       row.appendChild(button);
-
       // document.getElementsByTagName("button".onclick = function(){beginGame(a)}).innerHTML
     }
     board.appendChild(row);
-
+    
   }
   //loop within loop and make little boxes with bombs or numbers underneath and add them to the dom
+} 
+// function showEmpty(){
+// if()
+
+// }
+
+function markBomb(){
+
+    let toggleNum = Number(this.getAttribute('toggle'))
+    if(toggleNum%2===0){                            //if this is even - remove flag
+      this.innerHTML = `<div></div>`
+      document.getElementById("score").innerHTML++
+    } else{                                           //if odd - add the flag
+      this.innerHTML = `<div class="flagcol" id = "flagleft">
+      <div class = "flagrow" id="flagtriangle" > </div>
+      <div class = "flagrow"> </div>
+      </div>
+      <div class="flagcol" id = "flagstick">
+      </div>`
+      document.getElementById("score").innerHTML--
+    }
+    this.setAttribute('toggle',toggleNum+1)
 }
 
-beginGame([...arr]);
-makeField([...arr]);
+let seconds;
+let timer; 
+let firstClick = true;  //This happens on page load 
 
+
+function startClock(){
+  seconds = 0;
+  timer = setInterval(mytimer, 1000); //starts the timer to increase time every second
+}
+
+function mytimer(){
+  document.getElementById("timer").innerHTML = seconds;//places the time into the "timer" div and increments
+  seconds++;
+
+} 
 
 function doSomething() {
   console.log(this.value)
-  if (this.value === "B") {
-    alert("game over")
-    document.getElementById("boardDiv").innerHTML = ''
 
+  if (this.value === "B") { 
+    alert("game over")
+    document.getElementById("score").innerHTML = 10
+    document.getElementById("boardDiv").innerHTML = ''
+    firstClick=true;// starts timer upon first click if true
+    clearInterval(timer);
+  
     beginGame([...arr]);
     makeField([...arr]);
+    
 
     arr =
       [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -90,20 +167,27 @@ function doSomething() {
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
+      return
   }
-  this.innerHTML = this.value
+  this.innerHTML = `<span>${this.value}</span>`
   this.style.backgroundColor = 'blue'
-
+  if(firstClick === true){            //won't start the timer after further clicks
+    startClock()                      //Only once
+    firstClick = false; 
+  }
 }
-// function wireButtons(){
-//   document.getElementsByTagName('button').addEventListener("click", function(){
-//     console.log(this)
-//   });
-//   /*.onclick = function(){
-//     console.log(this)
-//   }*/
-// }
+
+document.getElementById("score").innerHTML = 10;
+
+function openCells(){
+  
+}
+
+
+beginGame([...arr]);
+makeField([...arr]);
+
+
 
 
 
